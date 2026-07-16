3e6c6362ba179c7f3be308531a416e8a95986f25
PRATHAMESHMESARE34 <165029687+PRATHAMESHMESARE34@users.noreply.github.com>
2026-07-16 14:48:03 +0530

    Create elements.js

---
+++ js/elements.js
@@ -0,0 +1,48 @@
+
+const audio = document.getElementById("audioPlayer");
+const playBtn = document.getElementById("playBtn");
+const previousBtn = document.getElementById("previousBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+const progressBar = document.getElementById("progressBar");
+const currentTime = document.getElementById("currentTime");
+const duration = document.getElementById("duration");
+
+const albumArt = document.getElementById("albumArt");
+const songTitle = document.getElementById("songTitle");
+const artistName = document.getElementById("artistName");
+
+const playlist = document.getElementById("playlist");
+
+const searchBox = document.getElementById("searchBox");
+
+const bpmSlider = document.getElementById("bpmSlider");
+const bpmValue = document.getElementById("bpmValue");
+
+const logo = document.getElementById("logo");
+const ownerLogin = document.getElementById("ownerLogin");
+const ownerPassword = document.getElementById("ownerPassword");
+const loginBtn = document.getElementById("loginBtn");
+
+const ownerDashboard = document.getElementById("ownerDashboard");
+const uploadModal = document.getElementById("uploadModal");
+
+const uploadSongsBtn = document.getElementById("uploadSongsBtn");
+
+
+
+
+const uploadTitle = document.getElementById("uploadTitle");
+const uploadArtist = document.getElementById("uploadArtist");
+const uploadBPM = document.getElementById("uploadBPM");
+
+const coverUpload = document.getElementById("coverUpload");
+const lyricsUpload = document.getElementById("lyricsUpload");
+
+const libraryType = document.getElementById("libraryType");
+
+const coverPreview = document.getElementById("coverPreview");
+
+
+
+
