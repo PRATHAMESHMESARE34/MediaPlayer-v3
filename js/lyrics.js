3d44d1eb57eca97bcdda4e62d3a193533bd306a8
PRATHAMESHMESARE34 <165029687+PRATHAMESHMESARE34@users.noreply.github.com>
2026-07-16 19:14:50 +0530

    Update lyrics.js

--- js/lyrics.js
+++ js/lyrics.js
@@ -1,1 +1,26 @@
 
+/*==================================================
+                LYRICS PANEL
+==================================================*/
+
+showLyricsBtn.addEventListener("click",()=>{
+
+    lyricsPanel.classList.add("show");
+
+});
+
+closeLyrics.addEventListener("click",()=>{
+
+    lyricsPanel.classList.remove("show");
+
+});
+
+lyricsPanel.addEventListener("click",(e)=>{
+
+    if(e.target===lyricsPanel){
+
+        lyricsPanel.classList.remove("show");
+
+    }
+
+});
